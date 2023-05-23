ab1d46d656c3bcd0ca418342f04966653a599eca
TonyKolia <tonykolia@gmail.com>
2023-05-23 15:03:40 +0300

    navbar logo changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,8 +17,9 @@ export default function CustomNavbar(props) {
 
     return (
         <Navbar id="navbar-container" fixed="top" expand="lg" expanded={expanded}>
+            
             <Container >
-                <Navbar.Brand href="/"><img style={{ "height": "60px" }} src={require("../Images/logo.png")} /></Navbar.Brand>
+            <Navbar.Brand href="/"><img style={{ "height": "70px" }} src={require("../Images/logo2.png")} /></Navbar.Brand>
                 <Navbar.Toggle id="navbar-toggler" onClick={() => setExpanded(oldValue => !oldValue)} aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto no-margin">
